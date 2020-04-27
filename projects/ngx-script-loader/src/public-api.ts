ce28649de7c9227476be12985c6aa5abc0bd0d6c
Rafa AS <rafaaguilar88@gmail.com>
2020-04-27 14:07:22 +0200

    renames

--- projects/ngx-script-loader/src/public-api.ts
+++ projects/ngx-script-loader/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-script-loader
  */
 
-export * from 'projects/ngx-script-loader/src/lib/script-loader.service';
-export * from 'projects/ngx-script-loader/src/lib/script-loader.component';
-export * from 'projects/ngx-script-loader/src/lib/script-loader.module';
+export * from './lib/script-loader.service';
+export * from './lib/script-loader.component';
+export * from './lib/script-loader.module';
